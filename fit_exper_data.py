3c602396e01108f2801b1359d8ca1159ba3d4550
wjj <williamjjohnston@gmail.com>
2020-08-13 12:29:49 -0500

    updated priors, similar to fits

--- fit_exper_data.py
+++ fit_exper_data.py
@@ -55,30 +55,30 @@ if __name__ == '__main__':
                'max_treedepth':args.max_treedepth}
     stan_params = {'iter':args.length, 'control':control, 'chains':args.chains}
     
-    rbmm = 5
-    rbmv = 10
-    rbvm = 2
+    rbmm = 6
+    rbmv = 6
+    rbvm = 3
     rbvv = 5
     
-    dbmm = rbmm
-    dbmv = rbmv
-    dbvm = rbvm
-    dbvv = rbvv
+    dbmm = 4
+    dbmv = 5
+    dbvm = 4
+    dbvv = 4
     
-    mdmm = 1
-    mdmv = 2
-    mdvm = .5
-    mdvv = .5
+    mdmm = .2
+    mdmv = .2
+    mdvm = .2
+    mdvv = .2
 
     enmm = 3
     enmv = 2
     envm = 2
     envv = 1
 
-    smmm = 3
-    smmv = 2
-    smvm = 2
-    smvv = 1
+    smmm = 4
+    smmv = 3
+    smvm = 3
+    smvv = 3
     
     lrbk = 7.5
     lrbt = .5

Fixed a numerical issue in AE rate integration

figures_of.py
@@ -573,9 +573,9 @@ def figure_fi(basefolder=bf, gen_panels=None, data=None):
     
     data_path = 'assignment/many_mse_tradeoffs-nr3.pkl'
     if data.get('de') is None and 'de' in gen_panels:
+        print('loading')
         out = pickle.load(open(data_path, 'rb'))
         data['de'] = out
-
         
     # iso_ax = f.add_subplot(iso_grid)
     # iso_neurs_ax = f.add_subplot(iso_neurs_grid)
@@ -586,8 +586,8 @@ def figure_fi(basefolder=bf, gen_panels=None, data=None):
         ax_vals, total_mse, mse_dist, ae_rate, mis_prob = data['de'][:5]
         total_units, n_feats, overlaps, total_pwrs = ax_vals
         # indices are (units, feats, overlaps, pwrs)
-        pwr_ind = 20
-        neurs_ind = 20
+        pwr_ind = 25
+        neurs_ind = 25
         feat_ind = 4
         print(n_feats[feat_ind], total_pwrs[pwr_ind])
         total_snrs = np.sqrt(total_pwrs)
@@ -604,6 +604,7 @@ def figure_fi(basefolder=bf, gen_panels=None, data=None):
         ov_thresh = 4
         for k, mse_r in mse_dist.items():            
             ae_r = ae_rate[k]
+            ae_r[ae_r > 1] = 1
             td_maps = []
             rep_feats = []
             for i, ov in enumerate(overlaps):
@@ -647,8 +648,9 @@ def figure_fi(basefolder=bf, gen_panels=None, data=None):
                 td_map = total_mse[k][:, feat_ind, i, :]
                 td_maps.append(td_map)
             full_map_k = np.stack(td_maps, axis=0)
-            # min_map_k = td_maps[0] # np.min(full_map_k, axis=0)
+            min_map_k = full_map_k[0] # np.min(full_map_k, axis=0)
             min_map_k = np.nanmin(full_map_k, axis=0)
+            
             min_maps.append(min_map_k)
             region_list.append(k)
         min_regions = np.stack(min_maps, axis=0)
@@ -707,12 +709,13 @@ def figure_fi(basefolder=bf, gen_panels=None, data=None):
 
 
         # indices are (units, feats, overlaps, pwrs)
-        pwr_ind = 30
-        neurs_ind = 30
+        pwr_ind = -1
+        neurs_ind = -1
         feat_ind = 4
         ov_ind = 0
         for i, (nr, mse_nr) in enumerate(mse_dist.items()):
             ae_nr = ae_rate[nr]
+            ae_nr[ae_nr > 1] = 1
             mse_nu = mse_nr[:, feat_ind, ov_ind, pwr_ind]
             col = colors[i]
             l = gpl.plot_trace_werr(total_units, nr*mse_nu, ax=mse_nu_ax,

overlapping_features.py
@@ -675,7 +675,7 @@ def mse_weighting(k, c, s, source_distrib='uniform', n_emp=10000):
         avg_dist = np.mean(d_rs)**2
     return avg_dist
 
-def integrate_assignment_error(esses, d1, d2, overlapping_d, p=100):
+def integrate_assignment_error(esses, d1, d2, overlapping_d, p=100, **kwargs):
     if d1 is None and d2 is not None:
         d1 = d2
     elif d2 is None and d1 is not None:
@@ -696,9 +696,9 @@ def integrate_assignment_error(esses, d1, d2, overlapping_d, p=100):
         dist_pdf = lambda x: line_picking_clt(x, overlapping_d)
     if overlapping_d <= 3 or overlapping_d > 10:
         ae = ae_integ(esses, d1, d2, p=p, integ_start=0, integ_end=integ_end,
-                      dist_pdf=dist_pdf)
+                      dist_pdf=dist_pdf, **kwargs)
     else:
-        ae = ae_sample(esses, d1, d2, overlapping_d, p=p)
+        ae = ae_sample(esses, d1, d2, overlapping_d, p=p, **kwargs)
     return ae
 
 def distance_error_rate(dists, delta_d, overall_d):
@@ -742,19 +742,25 @@ def ae_sample(esses, d1, d2, overlapping_d, p=1, n_samples=10**7, **kwargs):
     return errs        
 
 def ae_integ(esses, d1, d2, p=1, integ_start=0, integ_end=None, dist_pdf=None,
-             err_thr=.01, assert_=False):
+             err_thr=.01, assert_=False, use_factor=True):
     pes = np.zeros_like(d1)
     for i, d1_i in enumerate(d1):
         d2_i = d2[i]
+        if use_factor:
+            factor = 1/d1_i
+        else:
+            factor = 1
         def _f(x):
             v1 = dist_pdf(x)
             # v2 = sts.norm(x, np.sqrt(d1_i + d2_i)).cdf(0)
             v2 = sts.norm(x, np.sqrt(2*d1_i)).cdf(0)
             v3 = sts.norm(x, np.sqrt(2*d2_i)).cdf(0)
-            v = v1*(v2 + v3 - 2*v2*v3)
+            v = factor*v1*(v2 + v3 - 2*v2*v3)
             return v
-           
-        pes[i], err = sin.quad(_f, integ_start, integ_end)
+
+        out = sin.quad(_f, integ_start, integ_end)
+        pes[i] = out[0]/factor
+        err = out[1]
         if assert_:
             assert err < err_thr
         elif err > err_thr: